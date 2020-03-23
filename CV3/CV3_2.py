265317e8943bde654381d9e32507d7d3cc7dcc5c
z.svaton <z.svaton@kendaxa.com>
2020-03-23 17:23:03 +0100

    running knn for data2_x_tr.csv

--- CV3/CV3_2.py
+++ CV3/CV3_2.py
@@ -11,6 +11,7 @@ def knn(D, Y, k, num_samles = -1):
     :param D: Distance matrix
     :param Y: Labels
     :param k: k
+    :param num_samles: how many labels shoudl be randomly selected and checked
     :return: recalculated labels for each element
     """
     sample_size = Y.size
@@ -39,7 +40,7 @@ D = pairwise_distances(X, metric='manhattan')
 
 results = []
 for k in range(1, 20):
-    Y_pred, Y_indexes = knn(D, Y, k, num_samles=15000)
+    Y_pred, Y_indexes = knn(D, Y, k, num_samles=2500)
     f1 = f1_score(np.take(Y,Y_indexes).astype(int), Y_pred.astype(int), average='micro')
     results.append(f1)
     print("*** F1 for k:" + str(k) + " score: " + str(f1))
