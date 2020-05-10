404e3dd7a6924343ea7a53149183f65b1f36b29e
z.svaton <z.svaton@kendaxa.com>
2020-05-10 22:05:23 +0200

    CV5

---
+++ CV5/EnsembleKnn.py
@@ -0,0 +1,26 @@
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from  typing import List
+
+
+class EnsembleKnn(BaseEstimator, ClassifierMixin):
+    def __init__(self):
+        self.classifiers: List[KNeighborsClassifier] = []
+        self.f_scores = []
+
+    def fit(self, X, y,K):
+        classifiers = [ KNeighborsClassifier(n_neighbors=k) for k in range(1,K) ]
+        for c in classifiers:
+            c.fit(X,y)
+        return self
+
+    def predict(self, X):
+        y_res = None
+        for c in self.classifiers:
+           y_pred = c.predict(X)
+           if y_res is None:
+               y_res = np.copy(y_pred)
+           else:
+               y_res = np.row_stack((y_res, y_pred))
